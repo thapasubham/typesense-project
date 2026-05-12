edfb8173d421b807375337b8a92ac8982777f1ed
Subham Thapa <subhamits4.2@gmail.com>
2026-05-12 22:21:44 +0545

    extact data from csv

--- etl-pipeline/src/etl/extract.py
+++ etl-pipeline/src/etl/extract.py
@@ -7,7 +7,20 @@ logger = get_logger(__name__)
 
 
 def extract():
-    CSV_PATH = Path(__file__).parent.parent.parent / "dataset" / "links.csv"
+    files = ["movies", "links", "ratings", "tags"]
+    data = {}
 
-    for chunk in pd.read_csv(CSV_PATH, chunksize=100):
-        print(chunk.head())
+    dataset_path = Path("./dataset")
+
+    for file in files:
+        file_path = dataset_path / f"{file}.csv"
+        try:
+            df = pd.read_csv(file_path)
+            data[file] = df
+
+            logger.info(f"Loaded {file}.csv with {len(df)} rows")
+
+        except Exception as e:
+            logger.error(f"Failed to load {file}.csv: {e}")
+    logger.info("Data extraction complete")
+    return data
